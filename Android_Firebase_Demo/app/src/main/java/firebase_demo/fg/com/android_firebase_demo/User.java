0b8946cc4a82684e99d89c80ba3f62b3708b0191
Florian Gabach <florian.gabach@gmail.com>
2016-12-09 08:44:39 +0100

    Clean

--- Android_Firebase_Demo/app/src/main/java/firebase_demo/fg/com/android_firebase_demo/User.java
+++ Android_Firebase_Demo/app/src/main/java/firebase_demo/fg/com/android_firebase_demo/User.java
@@ -13,16 +13,7 @@ public class User {
         this.id = id;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getId() {
-
         return id;
     }
 
