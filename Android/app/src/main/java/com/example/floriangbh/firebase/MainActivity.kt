331d25e7159af309123cbb35ae979441ef84e170
Florian Gabach <florian.gabach@gmail.com>
2019-07-23 16:21:12 +0200

    Android : Bump graddle, AndroixX, dependencies

--- Android/app/src/main/java/com/example/floriangbh/firebase/MainActivity.kt
+++ Android/app/src/main/java/com/example/floriangbh/firebase/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.example.floriangbh.firebase
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.LinearLayoutManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.database.DatabaseReference
 
 import kotlinx.android.synthetic.main.activity_main.*
